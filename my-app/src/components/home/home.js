927ae972f1cee550c6b90eacb5bfe675a0dd3417
Ashley Roarick <ahorseroar@gmail.com>
2021-01-01 17:36:08 -0600

    starting over

--- my-app/src/components/home/home.js
+++ my-app/src/components/home/home.js
@@ -2,10 +2,8 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import CardColumns from "react-bootstrap/CardColumns";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-
-import Navigation from "../navigation/navigation";
 
 import "../../App.scss";
 
